afdbe0e62bdc12a459b1d92cd037e9f6e7c2b0c6
JonnyJong <jonnyjong@outlook.com>
2024-01-09 21:50:49 +0800

    ✨ feat(style): config getter

--- tools/compiler/style.js
+++ tools/compiler/style.js
@@ -4,6 +4,25 @@ const path = require('path');
 const { readFile } = require('fs/promises');
 const { src2dist } = require('../shared/path');
 
+function getConfig(file, keys) {
+  if (typeof file !== 'string') return;
+  if (typeof keys !== 'string') return;
+
+  let config;
+  try {
+    config = require(__dirname, '../../src/shared/config', file + '.json');
+  } catch {
+    return;
+  }
+
+  for (const key of keys.split('.')) {
+    if (typeof config !== 'object') return;
+    config = config[key];
+  }
+
+  return config;
+}
+
 async function compileStyle(file) {
   const source = await readFile(file, 'utf8');
   try {
@@ -11,6 +30,9 @@ async function compileStyle(file) {
     const css = render(source, {
       paths: [dir],
       filename: base,
+      globals: {
+        config: getConfig,
+      },
     });
     await writeFile(src2dist(file, name + '.css'), css, 'utf8');
   } catch (error) {
@@ -21,7 +43,7 @@ async function compileStyle(file) {
 module.exports = { compileStyle };
 
 if (require.main === module) {
-  eachInDir('./src/renderer', (file)=>{
+  eachInDir('./src', (file)=>{
     if (path.parse(file).ext !== '.styl') return;
     return compileStyle(file);
   });
