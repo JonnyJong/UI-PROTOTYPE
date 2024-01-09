4df6003ef5ec3b91b699dabc2a8798ef766b4aa6
JonnyJong <jonnyjong@outlook.com>
2024-01-09 16:03:19 +0800

    ✨ feat(main/utils): get data path

---
+++ src/main/utils/path.ts
@@ -0,0 +1,15 @@
+import { app } from "electron";
+import path from "path";
+
+/**
+ * Retrieves the path for a specified data file.
+ * @param filename - The name of the data file for which to retrieve the path.
+ * @returns - The complete path of the data file.
+ */
+export function getDataPath(filename: string): string {
+  return path.join(
+    app.getPath('appData'),
+    app.getName(),
+    filename,
+  );
+}
