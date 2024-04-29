fa42fb116b242004d0222c254dcd19b52065efb8
Jonny <jonnyjong@outlook.com>
2024-04-29 19:10:59 +0800

    ✨ feat(UI): Basic template

---
+++ src/renderer/main/main.ts
@@ -0,0 +1,46 @@
+import { ipcRenderer } from 'electron';
+import { initMica } from 'shared/ui/background';
+import { initAllComponents } from 'shared/ui/components';
+import { loadLocales } from 'shared/ui/locale';
+import { titlebar } from 'shared/ui/titlebar';
+import { initWindowEvents } from 'shared/ui/window';
+import { $ } from 'shared/utils/dom';
+
+document.addEventListener('DOMContentLoaded', async () => {
+  initWindowEvents();
+  initMica();
+  await loadLocales();
+  initAllComponents();
+  titlebar.init();
+});
+
+// XXX: Dev functions, remove before release
+ipcRenderer.invoke('dev').then((isDev) => {
+  if (!isDev) return;
+  window.addEventListener('keypress', ({ code, ctrlKey }) => {
+    if (ctrlKey && code === 'KeyR') location.reload();
+  });
+  window.addEventListener('keydown', ({ ctrlKey, key }) => {
+    if (!ctrlKey) return;
+    switch (key) {
+      case '=':
+        ipcRenderer.send('dev:scale', 0.1);
+        break;
+      case '-':
+        ipcRenderer.send('dev:scale', -0.1);
+        break;
+      case '0':
+        ipcRenderer.send('dev:resetScale');
+        break;
+    }
+  });
+  ipcRenderer.on('dev:css', () => {
+    $<HTMLLinkElement>('link[rel="stylesheet"]').forEach((link) => {
+      let newLink = $.new('link');
+      newLink.rel = 'stylesheet';
+      newLink.href = link.href.split('?')[0] + '?' + Date.now();
+      link.before(newLink);
+      link.remove();
+    });
+  });
+});
