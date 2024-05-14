5df020b6e4a5c8a50d30c2a5b7c3b945a0ff93f1
Jonny <jonnyjong@outlook.com>
2024-05-14 21:35:37 +0800

    🐞 fix(Utils): unid not unique

--- src/shared/utils/id.ts
+++ src/shared/utils/id.ts
@@ -1,9 +1,12 @@
-const start = new Date(2024, 0, 1).getTime();
-const randomBytes = Math.pow(36, 2);
+const startTime = new Date(2024, 0, 1).getTime();
+const randomBytesLength = 2;
+const randomBytesSize = Math.pow(36, randomBytesLength);
 
 export function unid(): string {
   return (
-    (Date.now() - start).toString(36) +
-    Math.floor(Math.random() * randomBytes).toString(36)
+    (Date.now() - startTime).toString(36) +
+    Math.floor(Math.random() * randomBytesSize)
+      .toString(36)
+      .padStart(randomBytesLength, '0')
   );
 }
