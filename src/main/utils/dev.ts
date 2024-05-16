8cf8e96bc62858c0f766333703fb6794287f4076
Jonny <jonnyjong@outlook.com>
2024-05-16 14:47:56 +0800

    ✨ feat(Dev): auto rereload

--- src/main/utils/dev.ts
+++ src/main/utils/dev.ts
@@ -2,6 +2,7 @@ import { WebContents, app, ipcMain } from 'electron';
 import { watchFile } from 'fs';
 import { readdir } from 'fs/promises';
 import path from 'path';
+import { sleep } from 'shared/utils/timer';
 
 const distRoot = path.join(__dirname, '../../');
 
@@ -40,9 +41,29 @@ function initScale(ctx: WebContents) {
   });
 }
 
+function initAutoReReload(ctx: WebContents) {
+  let lastLoad = Date.now();
+  let tryTimes = 0;
+  ctx.on('did-start-loading', () => {
+    if (Date.now() - lastLoad > 5000) {
+      tryTimes = 0;
+    }
+    lastLoad = Date.now();
+  });
+  ctx.on('preload-error', async (_, __, error) => {
+    if (!error.message.startsWith('Cannot find module')) return;
+    if (Date.now() - lastLoad > 1000) return;
+    if (tryTimes > 10) return;
+    await sleep(1000);
+    ctx.reload();
+    tryTimes++;
+  });
+}
+
 export function initDevFunctions(ctx: WebContents, label: string) {
   if (app.isPackaged) return;
   ctx.openDevTools();
   initAutoReloadCSS(ctx, label);
   initScale(ctx);
+  initAutoReReload(ctx);
 }
